b0bdf83f305fbe3ca7136495be184d71ece4e817
CHRISDAVIDSPICER <CHRISDAVIDSPICER@GMAIL.COM>
2021-04-09 16:30:05 -0700

    portfolio changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
 import '../styles/globals.css'
+import Navbar from '../components/Navbar.jsx'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Navbar />
+
+      <Component {...pageProps} />
+
+      {/* footer would go here */}
+    </>
+  )
 }
 
 export default MyApp
